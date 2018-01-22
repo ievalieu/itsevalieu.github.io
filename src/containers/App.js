65b4c2ff734b4a48006a3165db59fe1b25afde7d
itsevalieu <itsevalieu@gmail.com>
2018-01-21 23:08:17 -0800

    Adding state folderClicked

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,6 +9,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      folderClicked: false,
       aboutClicked: false,
       portfolioClicked: false,
       contactClicked: false
