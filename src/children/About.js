3c82634812825ee2e5d12c80a6ba07d1eb42776b
itsevalieu <itsevalieu@gmail.com>
2018-01-31 23:54:37 -0800

    Adding more content to About

--- src/children/About.js
+++ src/children/About.js
@@ -11,13 +11,12 @@ class About extends Component {
 				</div>
 				<div className="about-biography">
 					<p>Present: Eva Lieu is a web developer versed in data science and data visualization. She has worked previously worked in at a tech startup called SueYa as a front-end web developer and is currently working at a tea company called Bird Pick Tea & Herb as their General and Operations Manager.</p>
-					<p>Past</p>
-					<p>Future</p>
-					<p>Interests</p>
+					<p>Past: UCLA Alumni, English Major and Labor and Workplace Studies minor; Coding Bootcamp with UCLA Extension for Full Stack Web development</p>
+					<p>Interests: Data Visualization, Public Policy, Business Logic, Storytelling</p>
 				</div>
 				<div className="about-info">
-					<p>Skills</p>
-					<p>Experience</p>
+					<p>Skills: HTML3, CSS3, Javascript, Node.js, Express, React, MySQL, MongoDB, Python, APIs</p>
+					<p>Experience: Sueya, Bird Pick Tea & Herb, Teafographic</p>
 				</div>
 			</div>
 		);
